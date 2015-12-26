2329098a5aac14bd0654c29ccfd045ec17b9c9ba
Richard Alex Hofer <me@rofer.me>
2015-12-26 15:13:17 -0500

    Refactor to allow values in addition to keys.

--- PairingHeap.elm
+++ PairingHeap.elm
@@ -3,27 +3,27 @@ module PairingHeap (PairingHeap, empty, findMin, merge, insert, deleteMin, fromL
 import Maybe exposing (Maybe(..))
 
 
-type PairingHeap comparable
+type PairingHeap comparable a
     = Empty
-    | Heap comparable (List (PairingHeap comparable))
+    | Heap comparable a (List (PairingHeap comparable a))
 
 
-empty : PairingHeap comparable
+empty : PairingHeap comparable a
 empty =
     Empty
 
 
-findMin : PairingHeap comparable -> Maybe comparable
+findMin : PairingHeap comparable a -> Maybe (comparable, a)
 findMin x =
     case x of
         Empty ->
             Nothing
 
-        Heap x _ ->
-            Just x
+        Heap k v _ ->
+            Just (k, v)
 
 
-merge : PairingHeap comparable -> PairingHeap comparable -> PairingHeap comparable
+merge : PairingHeap comparable a -> PairingHeap comparable a -> PairingHeap comparable a
 merge heap1 heap2 =
     case ( heap1, heap2 ) of
         ( Empty, _ ) ->
@@ -32,29 +32,29 @@ merge heap1 heap2 =
         ( _, Empty ) ->
             heap1
 
-        ( Heap x xs, Heap y ys ) ->
-            if x < y then
-                Heap x (heap2 :: xs)
+        ( Heap k1 v1 hs1, Heap k2 v2 hs2 ) ->
+            if k1 < k2 then
+                Heap k1 v1 (heap2 :: hs1)
             else
-                Heap y (heap1 :: ys)
+                Heap k2 v2 (heap1 :: hs2)
 
 
-insert : comparable -> PairingHeap comparable -> PairingHeap comparable
-insert x heap =
-    merge (Heap x []) heap
+insert : (comparable, a) -> PairingHeap comparable a -> PairingHeap comparable a
+insert (k, v) heap =
+    merge (Heap k v []) heap
 
 
-deleteMin : PairingHeap comparable -> PairingHeap comparable
+deleteMin : PairingHeap comparable a -> PairingHeap comparable a
 deleteMin heap =
     case heap of
         Empty ->
             Empty
 
-        Heap x heaps ->
+        Heap k v heaps ->
             mergePairs heaps
 
 
-mergePairs : List (PairingHeap comparable) -> PairingHeap comparable
+mergePairs : List (PairingHeap comparable a) -> PairingHeap comparable a
 mergePairs heaps =
     case heaps of
         [] ->
@@ -71,16 +71,16 @@ mergePairs heaps =
 -- Extra convenience functions
 
 
-fromList : List comparable -> PairingHeap comparable
+fromList : List (comparable, a) -> PairingHeap comparable a
 fromList =
     List.foldl insert empty
 
 
-toSortedList : PairingHeap comparable -> List comparable
+toSortedList : PairingHeap comparable a -> List (comparable, a)
 toSortedList heap =
     case heap of
         Empty ->
             []
 
-        Heap x _ ->
-            x :: (toSortedList (deleteMin heap))
+        Heap k v _ ->
+            (k, v) :: (toSortedList (deleteMin heap))
--- Tests.elm
+++ Tests.elm
@@ -8,23 +8,23 @@ import Graphics.Element exposing (Element)
 
 
 h1 =
-    insert 5 empty
+    insert (5, ()) empty
 
 
 h2 =
-    insert 10 h1
+    insert (10, ()) h1
 
 
 h3 =
-    insert 1 h2
+    insert (1, ()) h2
 
 
 h4 =
-    insert 7 empty
+    insert (7, ()) empty
 
 
 h5 =
-    insert 2 h4
+    insert (2, ()) h4
 
 
 tests : Test
@@ -41,8 +41,8 @@ tests =
                 , defaultTest (assertEqual (merge h2 empty) h2)
                 , defaultTest (assertEqual (merge empty h3) h3)
                 ]
-            , defaultTest (assertEqual (findMin (insert 42 empty)) (Just 42))
-            , defaultTest (assertEqual (deleteMin (insert 42 empty)) empty)
+            , defaultTest (assertEqual (findMin (insert (42, True) empty)) (Just (42, True)))
+            , defaultTest (assertEqual (deleteMin (insert (42, False) empty)) empty)
             ]
         , suite
             "Non-empty Tests"
@@ -55,14 +55,16 @@ tests =
                 , defaultTest (assertEqual (heapsort [ 2, 3, 1 ]) [ 1, 2, 3 ])
                 , defaultTest (assertEqual (heapsort [ 5, 2, 3, 1, 4 ]) [ 1, 2, 3, 4, 5 ])
                 ]
-            , test "Merge test" (assertEqual (toSortedList (merge h3 h5)) [ 1, 2, 5, 7, 10 ])
+            , test "Merge test" (assertEqual 
+                (toSortedList (merge h3 h5)) 
+                [ (1, ()), (2, ()), (5, ()), (7, ()), (10, ()) ])
             ]
         ]
 
 
 heapsort : List comparable -> List comparable
 heapsort =
-    fromList >> toSortedList
+    (List.map (\x -> (x, ()))) >> fromList >> toSortedList >> (List.map (\(k,_) -> k))
 
 
 main : Element
