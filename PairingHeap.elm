7eb6876069941284e6a5f9f59c7ef46204e0d73a
Richard Alex Hofer <me@rofer.me>
2016-01-12 20:01:31 -0500

    Add inline documentation

--- PairingHeap.elm
+++ PairingHeap.elm
@@ -1,18 +1,41 @@
 module PairingHeap (PairingHeap, empty, findMin, merge, insert, deleteMin, fromList, toSortedList) where
 
+{-| This is a simple pairing heap implementation written in Elm usable as a priority queue. This code is
+based heavily on the pseudocode available at [the Wikipedia page](https://en.wikipedia.org/wiki/Pairing_heap).
+
+# Type and Constructor
+@docs PairingHeap, empty
+
+# Operations
+@docs insert, merge, findMin, deleteMin
+
+# Convenience functions
+@docs fromList, toSortedList
+-}
+
 import Maybe exposing (Maybe(..))
 
 
+{-| A `PairingHeap` has comparable keys and values of an arbitrary type.
+-}
 type PairingHeap comparable a
     = Empty
     | Heap comparable a (List (PairingHeap comparable a))
 
 
+{-| Create an empty PairingHeap.
+-}
 empty : PairingHeap comparable a
 empty =
     Empty
 
 
+{-| Find the minimum value in a heap returning Nothing if the heap is empty.
+
+Complexity: O(1)
+
+    findMin (fromList [(10, ()), (3, ()), (8, ())]) == Just 3
+-}
 findMin : PairingHeap comparable a -> Maybe (comparable, a)
 findMin x =
     case x of
@@ -23,6 +46,10 @@ findMin x =
             Just (k, v)
 
 
+{-| Merges two `PairingHeap`s together into one new heap containing all of the key-value pairs from both inputs.
+
+Complexity: O(1)
+-}
 merge : PairingHeap comparable a -> PairingHeap comparable a -> PairingHeap comparable a
 merge heap1 heap2 =
     case ( heap1, heap2 ) of
@@ -39,11 +66,20 @@ merge heap1 heap2 =
                 Heap k2 v2 (heap1 :: hs2)
 
 
+{-| Inserts a new element into a `PairingHeap`.
+
+Complexity: O(1)
+-}
 insert : (comparable, a) -> PairingHeap comparable a -> PairingHeap comparable a
 insert (k, v) heap =
     merge (Heap k v []) heap
 
 
+{-| Removes the minimum element from a `PairingHeap` returning a new heap without that element.
+This will return an empty heap if given an empty heap as input.
+
+Complexity: O(log n)
+-}
 deleteMin : PairingHeap comparable a -> PairingHeap comparable a
 deleteMin heap =
     case heap of
@@ -54,6 +90,8 @@ deleteMin heap =
             mergePairs heaps
 
 
+{-| This is an internal function used by deleteMin.
+-}
 mergePairs : List (PairingHeap comparable a) -> PairingHeap comparable a
 mergePairs heaps =
     case heaps of
@@ -71,11 +109,19 @@ mergePairs heaps =
 -- Extra convenience functions
 
 
+{-| This function turns a list of key-value pairs into a `PairingHeap`.
+
+Complexity: O(n)
+-}
 fromList : List (comparable, a) -> PairingHeap comparable a
 fromList =
     List.foldl insert empty
 
 
+{-| This function turns a `PairingHeap` into a sorted list of key-value pairs.
+
+Complexity: O(n log n)
+-}
 toSortedList : PairingHeap comparable a -> List (comparable, a)
 toSortedList heap =
     case heap of
